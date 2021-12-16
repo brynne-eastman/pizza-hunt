2717acd7a5eb6670f4bc438ebcfce710d48bf84d
Brynne Eastman <brynne.eastman@gmail.com>
2021-12-16 06:43:00 -0500

    added replies to comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,6 +22,34 @@ const commentController = {
       .catch(err => res.json(err));
   },
 
+  //add replies to comments
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found with this id!' });
+        return;
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => res.json(err));
+  },
+
+  //remove replies
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
+  },
+
   // remove comment
   removeComment({ params }, res) {
     Comment.findOneAndDelete({ _id: params.commentId })
@@ -44,6 +72,7 @@ const commentController = {
       })
       .catch(err => res.json(err));
   }
+
 };
 
 module.exports = commentController;
